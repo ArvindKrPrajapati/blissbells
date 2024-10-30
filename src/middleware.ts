b8495285ad782d6cd6d42664ae11b2197f36465d
ArvindKrPrajapati <arvindsoft37@gmail.com>
2024-10-30 18:40:02 +0530

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,23 +15,17 @@ export async function middleware(request: NextRequest) {
   const auth = getServerCookies();
   const route = request.nextUrl.pathname;
   const url = new URL(request.url);
-  if (auth) {
-    if (url.searchParams.get("auth")) {
-      url.searchParams.delete("auth");
-      return NextResponse.redirect(url);
+  for (let r of protectedRoutes) {
+    if (!auth) {
+      if (route.includes(r)) {
+        const isExists = url.searchParams.get("auth");
+        if (isExists) {
+          return NextResponse.next();
+        }
+        return NextResponse.redirect(new URL("/?auth=true", request.url));
+      }
     }
   }
-  // for (let r of protectedRoutes) {
-  //   if (!auth) {
-  //     if (route.includes(r)) {
-  //       const isExists = url.searchParams.get("auth");
-  //       if (isExists) {
-  //         return NextResponse.next();
-  //       }
-  //       return NextResponse.redirect(new URL("/?auth=true", request.url));
-  //     }
-  //   }
-  // }
 
   return NextResponse.next();
 }
