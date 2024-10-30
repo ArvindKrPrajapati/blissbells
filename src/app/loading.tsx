479c73058361381b234c8e5464a01df62f3a1a51
ArvindKrPrajapati <arvindsoft37@gmail.com>
2024-10-30 16:20:22 +0530

    modified loader comp

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,14 +1,18 @@
+import ActionLoader from "@/components/ActionLoader";
 import { Progress } from "@nextui-org/react";
 import React from "react";
 
 export default function Loading() {
   return (
-    <Progress
-      size="sm"
-      isIndeterminate
-      aria-label="Loading..."
-      className="fixed top-0 w-full left-0"
-      style={{ zIndex: 1000 }}
-    />
+    <>
+      <Progress
+        size="sm"
+        isIndeterminate
+        aria-label="Loading..."
+        className="fixed top-0 w-full left-0"
+        style={{ zIndex: 1000 }}
+      />
+      <ActionLoader />
+    </>
   );
 }
