565c72f0596a8aa78386fad8821fac0dd1b702a3
ArvindKrPrajapati <arvindsoft37@gmail.com>
2024-10-31 01:22:00 +0530

    bug fix

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,17 +4,18 @@ import React from "react";
 import MobileNumber from "./MobileNumber";
 import UserDetailsCard from "./UserDetailsCard";
 import LogoutButton from "@/components/Auth/LogoutButton";
+import Container from "@/components/Container";
 
 export default async function ProfilePage() {
   const auth = getServerCookies();
   const data = await apiGet(`/users/${auth?.user?.id}`, auth);
   return (
-    <div className="p-3">
+    <Container className="p-3">
       <UserDetailsCard data={data} />
       <br />
       <MobileNumber data={data} />
       <br />
       <LogoutButton />
-    </div>
+    </Container>
   );
 }
