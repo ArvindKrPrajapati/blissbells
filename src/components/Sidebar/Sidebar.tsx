a61192caf763aeb46acd788c1fc892e62c89d119
ArvindKrPrajapati <arvindsoft37@gmail.com>
2024-10-31 12:54:44 +0530

    color changes

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -17,7 +17,7 @@ export default function Sidebar({ routes }: props) {
     <>
       <Accordion
         isCompact={true}
-        className="animate-[appearance-in_400ms] md:hidden bg-red-400 mb-2"
+        className="animate-[appearance-in_400ms] md:hidden bg-red-500 mb-2"
         variant="light"
         selectedKeys={isOpen ? ["1"] : []}
         onSelectionChange={(keys: any) => setIsOpen(keys.size > 0)}
@@ -25,12 +25,12 @@ export default function Sidebar({ routes }: props) {
         <AccordionItem
           key="1"
           title={
-            <p className="text-sm font-medium">
+            <p className="text-sm font-medium text-gray-100">
               {routes.find((i) => i.route == pathname)?.name}
             </p>
           }
           indicator={
-            <i className="fa-solid fa-chevron-down text-zinc-900 text-xs" />
+            <i className="fa-solid fa-chevron-down text-white text-xs" />
           }
           aria-label={"menu"}
           className="px-3"
@@ -40,7 +40,7 @@ export default function Sidebar({ routes }: props) {
               key={index}
               href={`${item.route}`}
               onClick={() => setIsOpen(false)}
-              className={`mb-2 block px-3 py-2 text-[0.95rem] hover:bg-gray-100 ${pathname == item.route ? "bg-gray-100 text-zinc-800" : "bg-inherit text-zinc-900"} rounded-md font-medium transition-all duration-300`}
+              className={`mb-2 block px-3 py-2 text-[0.95rem] hover:bg-red-400 ${pathname == item.route ? "bg-red-400" : ""} text-zinc-100 rounded-md font-medium transition-all duration-300`}
             >
               <i className={`${item.icon} mr-2`} />
               {item.name}
@@ -55,7 +55,7 @@ export default function Sidebar({ routes }: props) {
             <Link
               key={index}
               href={`${item.route}`}
-              className={`px-3 py-2 text-[0.95rem] hover:bg-zinc-200 hover:text-zinc-800 ${pathname == item.route ? "bg-zinc-200 text-zinc-800" : "bg-inherit text-zinc-400"} rounded-md font-medium transition-all duration-300`}
+              className={`px-3 py-2 text-[0.95rem] hover:bg-red-200 hover:text-red-800 ${pathname == item.route ? "bg-red-200 text-red-800" : "bg-inherit text-red-400"} rounded-md font-medium transition-all duration-300`}
             >
               <i className={`${item.icon} mr-2`} />
               {item.name}
